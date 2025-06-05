779b89672fda144ab08ca620df9e6cadd7dc5936
Harsh122002.git <chavadaharsh22@gmail.com.git>
2025-06-05 13:02:13 +0530

    database

--- config/db.js
+++ config/db.js
@@ -8,6 +8,8 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+                  family: 4,
+
     });
     console.log("MongoDB connected...");
   } catch (err) {
